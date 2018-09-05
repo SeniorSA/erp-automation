509efe9e36927a05f838dd39b9382b439ffc9522
marcel.fortunato <marcel.fortunato@senior.com.br>
2018-09-05 15:45:12 -0300

    Teste permissão

--- gestao-empresarial-cenarios/src/test/java/com/senior/suite/ExecutarTodosOsTestes.java
+++ gestao-empresarial-cenarios/src/test/java/com/senior/suite/ExecutarTodosOsTestes.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-//teste
 @RunWith(Suite.class)
 @SuiteClasses({
 	SuiteDeTestesCadastros.class,
