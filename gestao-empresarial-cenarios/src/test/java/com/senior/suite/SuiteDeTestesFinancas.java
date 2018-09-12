ffc3ee5f8fd1688573de6bb1558bad5cf0444de1
copagril-erp-automation <janio.santana@senior.com.br>
2018-09-12 17:26:56 -0300

    teste

--- gestao-empresarial-cenarios/src/test/java/com/senior/suite/SuiteDeTestesFinancas.java
+++ gestao-empresarial-cenarios/src/test/java/com/senior/suite/SuiteDeTestesFinancas.java
@@ -8,6 +8,8 @@ import senior.erp.testes.financas.cobrancaEscritural.TCFRCRP0001_RemessaCR;
 import senior.erp.testes.financas.contasPagar.TCFPCP0001_EntradaManutencaoTitulosCP;
 import senior.erp.testes.financas.contasPagar.TCFPCP0002_BaixasTitulosCP_Dinheiro_Cheque;
 import senior.erp.testes.financas.contasPagar.TCFPCP0003_CompensacaoTCP;
+import senior.erp.testes.financas.contasReceber.TCFRCR0001_EntradaManual;
+import senior.erp.testes.financas.contasReceber.TCFRCR0002_AlteracaoVencimentoCR;
 import senior.erp.testes.financas.contasReceber.TCFRCR0007_InclusaoChequeApop;
 import senior.erp.testes.financas.contasReceber.TCFRCR0008_EntradaTitulosCR;
 
@@ -21,6 +23,8 @@ import senior.erp.testes.financas.contasReceber.TCFRCR0008_EntradaTitulosCR;
 	TCFPCP0001_EntradaManutencaoTitulosCP.class,
 	TCFPCP0002_BaixasTitulosCP_Dinheiro_Cheque.class,
 	TCFPCP0003_CompensacaoTCP.class,
+	TCFRCR0001_EntradaManual.class,
+	TCFRCR0002_AlteracaoVencimentoCR.class,
 	TCFRCR0007_InclusaoChequeApop.class,
 	TCFRCR0008_EntradaTitulosCR.class,
 })
