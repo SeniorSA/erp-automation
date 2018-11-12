84e23768cd3bb47a92628e1c6c892c17201efd75
copagril-erp-automation <janio.santana@senior.com.br>
2018-11-12 17:14:22 -0200

    a

--- gestao-empresarial-cenarios/src/test/java/com/senior/suite/SuiteDeTestesSuprimentos.java
+++ gestao-empresarial-cenarios/src/test/java/com/senior/suite/SuiteDeTestesSuprimentos.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import senior.erp.testes.suprimentos.entradaBalanca.TCSREB0002_EntradaBalancaViaOrdemCompraAGRO2;
+import senior.erp.testes.suprimentos.estoque.TCSEST0001_TransDepositoFilial;
 import senior.erp.testes.suprimentos.faturamentoDoLeite.TCSUP0001_ValidarNotaLeite;
 import senior.erp.testes.suprimentos.notasFiscaisEntrada.TCSRNF0015_NotasFiscaisEntradaAgrupada;
 import senior.erp.testes.suprimentos.solicitacaoCompra.TCSCSC0001_SolicitacaoCompraProduto;
@@ -19,6 +20,7 @@ import senior.erp.testes.suprimentos.solicitacaoCompra.TCSCSC0001_SolicitacaoCom
 	TCSUP0001_ValidarNotaLeite.class,
 	TCSRNF0015_NotasFiscaisEntradaAgrupada.class,
 	TCSCSC0001_SolicitacaoCompraProduto.class,
+	TCSEST0001_TransDepositoFilial.class,
 })
 public class SuiteDeTestesSuprimentos {
 
