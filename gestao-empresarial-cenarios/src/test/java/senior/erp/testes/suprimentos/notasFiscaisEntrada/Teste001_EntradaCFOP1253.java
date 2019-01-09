f75740b2a10f0c57d54f16d53b7af6a1e215e9c9
copagril-erp-automation <janio.santana@senior.com.br>
2019-01-09 14:22:02 -0200

    aa

--- gestao-empresarial-cenarios/src/test/java/senior/erp/testes/suprimentos/notasFiscaisEntrada/Teste001_EntradaCFOP1253.java
+++ gestao-empresarial-cenarios/src/test/java/senior/erp/testes/suprimentos/notasFiscaisEntrada/Teste001_EntradaCFOP1253.java
@@ -6,11 +6,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.senior.framework.testes.CaixaAtribuicao;
 import com.senior.framework.testes.SistemaSenior;
 import com.senior.framework.testes.SistemaSeniorComTransacao;
 import com.senior.framework.testes.Tecla;
 
+import senior.erp.MetodosComuns;
 import senior.erp.MetodosComunsNucleo;
 import senior.erp.SystemName;
 import senior.erp.SystemUsers;
@@ -39,6 +39,7 @@ public class Teste001_EntradaCFOP1253 {
 
 	@Test 
 	public void testScenario01(){ 
+    MetodosComuns.selecionarEmpresaFilial(1, 1);		
 	SistemaSenior.abrirTela("NF440GNE_SRNF");
 	SistemaSenior.teclar(Tecla.TAB);
 	SistemaSenior.preencherCampo("ECgcCpf", "4368898000106");
@@ -51,16 +52,34 @@ public class Teste001_EntradaCFOP1253 {
 	SistemaSenior.teclar(Tecla.TAB);
 	SistemaSenior.preencherCampo("ETnsSer", "1253");
 	SistemaSenior.selecionarGuia("TNItens", "Serviços");
-	SistemaSenior.preencherLinhaCorrenteGrade("GridSer", "Transação","1253","Filial O.C.","0","Serviço","70021.0007","Qtde. Recebida","1","Preço Unitário","1000");
-	SistemaSenior.preencherLinhaCorrenteGrade("GridSer", "Valor Desconto","200");
-	SistemaSenior.posicionarLinhaGradePorValor("SwwDBGridContas","Financeira","1.620","Contábil","0","Percentual","100,0000","Valor","800,00","Observações","");
+	SistemaSenior.preencherLinhaCorrenteGrade("GridSer", "Transação","1253","Filial O.C.","0","Serviço","70021.0007","Qtde. Recebida","1","Preço Unitário","1000","Valor Desconto","200");
+	SistemaSenior.posicionarLinhaGradePorValor("SwwDBGridContas","Financeira","1.620","Contábil","0");
 	SistemaSenior.clicar("Ok");//&Ok
 	SistemaSenior.selecionarGuia("TNItens", "Parcelas");
-	SistemaSenior.posicionarLinhaGradePorValor("GridPar","Parcela","1","Num Título","74101.01","Tipo Título","ENE");
+	SistemaSenior.posicionarLinhaGradePorValor("GridPar","Parcela","1","Num Título","74901.01","Tipo Título","ENE");
 	SistemaSenior.preencherLinhaCorrenteGrade("GridPar", "Vencimento","31/01/2025");
-	SistemaSenior.clicar("Fechar");//&Fechar
-	SistemaSenior.clicar("button1");//Este e um campo de mensagem que foi clicado em 'Sim' ou 'Nao'.
-	SistemaSenior.clicar("button1");//Este e um campo de mensagem que foi clicado em 'Sim' ou 'Nao'.
+	SistemaSenior.clicar("Fechar");//&Fechar	
+	SistemaSenior.conferirCaixaMensagem("Confirmação", "Confirma Fechamento da Nota Fiscal?", "&Sim");
+	SistemaSenior.conferirCaixaMensagem("Aviso", "Nota Fiscal Fechada com Sucesso. ", "Ok");
+	SistemaSenior.clicar("Sair");// &Sair	
+	
+	SistemaSeniorComTransacao.executarSQLQuery("select * from e440isc "
+			+ "where CodEmp = 1 "
+			+ "and CodFil = 1 "
+			+ "and NumNfc = 74901 "
+			+ "and CodSnf = '1' "
+			+ "and SeqIsc = 1"
+			+ "and PerIcm = 29",
+			1);	
+	
+	SistemaSeniorComTransacao.executarSQLQuery("select * from e440isc "
+			+ "where CodEmp = 1 "
+			+ "and CodFil = 1 "
+			+ "and NumNfc = 74901 "
+			+ "and CodSnf = '1' "
+			+ "and SeqIsc = 1"
+			+ "and vlrBic = 800",
+			1);		
+	
 	} 
-
 }
